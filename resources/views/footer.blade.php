f93f5e22e7cb793dac7bde89dd7f1a63e0c12408
fadeeva-marina <marinka24-97@mail.ru>
2017-11-22 09:13:56 +0400

    1

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+    <p class="tm-copyright-p">Copyright &copy; <span class="tm-current-year">2017</span> Your Company
+        | Designed by M</p>
+</footer>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    {{--<link rel="stylesheet" href="{{ asset('css/style.css') }}">--}}
+    {{--<link rel="stylesheet" type="text/css" href={{URL::asset('css/style.css')}}>--}}
+    {{--<link rel="stylesheet" type="text/css" href={{ URL::asset('public/css/style.css') }}>--}}
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+jgbjdgbjdgbdjgb
+</body>
+</html>
